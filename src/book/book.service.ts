9dae9f5ccf8f2cdf09c89848c8a896587c70983b
deepikadudala <deepika.dudala@sasi.ac.in>
2024-02-13 22:41:30 +0530

    Pagination and searching

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,13 +2,27 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schema/book.schema';
 import { Model } from 'mongoose';
+import { Query } from 'express-serve-static-core';
 
 @Injectable()
 export class BookService {
   constructor(@InjectModel(Book.name) private BookModel: Model<Book>) {}
 
-  async findAll(): Promise<Book[]> {
-    const books = await this.BookModel.find();
+  async findAll(query: Query): Promise<Book[]> {
+    const resPerPage = 2;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+    const books = await this.BookModel.find(keyword)
+      .limit(resPerPage)
+      .skip(skip);
     return books;
   }
 
