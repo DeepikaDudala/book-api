3dfe413fd83b1aa66a8348c601fd77274ac90a00
Deepika Siva Sahithi Dudala <128839850+DeepikaDudala@users.noreply.github.com>
2024-02-16 18:12:09 +0530

    authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { BookModule } from './book/book.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import { SahithiModule } from './sahithi/sahithi.module';
 
 @Module({
   imports: [
@@ -11,7 +10,6 @@ import { SahithiModule } from './sahithi/sahithi.module';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL),
     AuthModule,
-    SahithiModule,
   ],
 })
 export class AppModule {}
